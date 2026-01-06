0e37d1cb1c7cfb84a1cb1debafb9a744ade3af94
Roberto Etcheverria <etcheve@gmail.com>
2026-01-06 15:27:44 +0100

    template method desing pattern

---
+++ design_patterns/behavioral/template_method/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+class AbstractClass {
+    public: 
+    virtual ~AbstractClass() = default;
+    void templateMethod() const {
+        baseOperation1();   
+        requiredOperation1();
+        baseOperation2();
+        hook();
+        requiredOperation2();
+    }
+
+    void baseOperation1() const {
+        std::cout << "AbstractClass: baseOperation1\n";
+    }   
+    void baseOperation2() const {
+        std::cout << "AbstractClass: baseOperation2\n";
+    }
+    virtual void requiredOperation1() const = 0;
+    virtual void requiredOperation2() const = 0;
+    virtual void hook() const {}
+};
+
+
+class ConcreteClass1 : public AbstractClass {
+    public:
+    void requiredOperation1() const override {
+        std::cout << "ConcreteClass1: requiredOperation1\n";
+    }
+    void requiredOperation2() const override {
+        std::cout << "ConcreteClass1: requiredOperation2\n";
+    }
+};
+class ConcreteClass2 : public AbstractClass {
+    public:
+    void requiredOperation1() const override {
+        std::cout << "ConcreteClass2: requiredOperation1\n";
+    }
+    void requiredOperation2() const override {
+        std::cout << "ConcreteClass2: requiredOperation2\n";
+    }
+    void hook() const override {
+        std::cout << "ConcreteClass2: Overridden Hook\n";
+    }
+};
+
+int main(){
+    std::cout << "Same client code can work with different subclasses:\n";
+    AbstractClass* class1 = new ConcreteClass1();
+    class1->templateMethod();
+    delete class1;
+
+    AbstractClass* class2 = new ConcreteClass2();
+    class2->templateMethod();
+    delete class2;
+
+    return 0;
+}

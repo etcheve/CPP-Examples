4471df635040e23ba79e9cee4bfb8492c8a520c8
Roberto Etcheverria <etcheve@gmail.com>
2026-02-24 16:31:06 +0100

    coding game best time to buy

---
+++ coding_games/best_time_to_buy.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int cash = 0;
+        int hold = -prices[0];
+
+        for (int i = 1; i < prices.size(); ++i) {
+            cash = max(cash, hold + prices[i] - fee);
+            hold = max(hold, cash - prices[i]);
+        }
+        return cash;
+    }
+};

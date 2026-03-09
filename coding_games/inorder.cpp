5e32771914e9387f9281353877bb1cf3d520c71f
Roberto Etcheverria <etcheve@gmail.com>
2026-03-09 21:35:43 +0100

    [CG] inorder

---
+++ coding_games/inorder.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+#include <vector>
+using namespace std;    
+
+ // Definition for a binary tree node.
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        traversal(root, res);
+        return res;
+        
+    }
+    void traversal(TreeNode* root, vector<int>& res){
+        if(root != nullptr){
+            traversal(root->left, res);
+            res.push_back(root->val);
+            traversal(root->right,res) ;
+        }
+
+
+    }
+};

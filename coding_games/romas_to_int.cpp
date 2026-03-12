a96f7bccc2ed1f9d6a4a3335e1ca17e958366c63
Roberto Etcheverria <etcheve@gmail.com>
2026-03-12 14:28:35 +0100

    [CG] ROman to int

---
+++ coding_games/romas_to_int.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/roman-to-integer/
+// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+
+#include <unordered_map>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> value = {
+        {'I', 1}, {'V', 5}, {'X', 10},
+        {'L', 50}, {'C', 100},
+        {'D', 500}, {'M', 1000}
+    };
+
+    int total = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        if (i + 1 < s.size() && value[s[i]] < value[s[i + 1]]) {
+            total -= value[s[i]];
+        } else {
+            total += value[s[i]];
+        }
+    }
+
+    return total;
+    }
+};

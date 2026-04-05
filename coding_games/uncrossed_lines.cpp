57851bdd79a6ee3b42684ff6d97a626b81a2be29
Roberto Etcheverria <etcheve@gmail.com>
2026-04-06 01:23:57 +0200

    [CG] uncrossed lines

---
+++ coding_games/uncrossed_lines.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/uncrossed-lines/description/?envType=study-plan-v2&envId=dynamic-programming
+#include <vector>
+#include <algorithm>    
+using namespace std;
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+       vector<vector<int>> dp(nums1.size()+1, vector<int>(nums2.size()+1, 0));
+        int biggest = 0 ;
+        for (int i = 0; i < nums1.size(); ++i){
+            for(int j = 0 ; j < nums2.size(); ++j){
+                if(nums1[i]== nums2[j] ){
+                dp[i+1][j+1] = dp[i][j] +1;
+                biggest = max(biggest, dp[i+1][j+1]);
+                }
+                else{
+                    dp[i+1][j+1] = max(dp[i][j+1] ,dp[i+1][j] );
+                }
+            }
+
+        }
+return biggest;
+    }
+};

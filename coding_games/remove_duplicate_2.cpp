344d38d3450482a51c79b726b4fe1703f2c35c77
Roberto Etcheverria <etcheve@gmail.com>
2026-04-09 17:02:45 +0200

    [CG] remove duplicate 2

---
+++ coding_games/remove_duplicate_2.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
+#include <vector>
+using namespace std;    
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size()<2){
+            return nums.size();
+        }
+        int k = 2;
+        for (int i = 2; i < nums.size(); ++i) {
+            if ( nums[k-2] != nums[i] ) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};

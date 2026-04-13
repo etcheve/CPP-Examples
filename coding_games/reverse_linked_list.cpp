710a5c8c8713bcbb9a1aef60b6839e4c23c80580
Roberto Etcheverria <etcheve@gmail.com>
2026-04-13 12:45:16 +0200

    [CG] revers linked list

---
+++ coding_games/reverse_linked_list.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/reverse-linked-list/description/
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* next =nullptr;
+    if (head == nullptr){
+        return head;
+    }
+    while(head->next != nullptr){
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    head->next = prev;
+    return head;
+       
+    }
+};
